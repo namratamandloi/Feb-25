8b0eab88573d59055bb9db379678acdb0469d548
Namrata Mandloi <namratamandloi2211@gmail.com>
2025-06-10 16:51:49 +0530

    Assigment

---
+++ WJP/day3.1/src/main/java/com/cdac/pages/Servlet1.java
@@ -0,0 +1,40 @@
+package com.cdac.pages;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/test1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("in init of"+getClass());//F.Q class name
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		//get writer - PW
+		try(PrintWriter pw=response.getWriter()){
+			pw.print("Hello from"+getClass()+"at"+new Date());
+			
+		}
+	}
+
+}
